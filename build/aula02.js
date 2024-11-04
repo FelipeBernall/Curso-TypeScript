1f9491cece67e8be4f09adcd8ef1553db2217257
Felipe <igor.trovata@gmail.com>
2024-11-04 13:41:59 -0300

    Aula 2: Formas de compilar TypeScript para gerar o arquivo JavaScript.

---
+++ build/aula02.js
@@ -0,0 +1,5 @@
+"use strict";
+const nome = "Felipe";
+const idade = 21;
+console.log(nome);
+console.log(idade);
---
+++ src/aula02.ts
@@ -0,0 +1,10 @@
+// Como compiliar o TypeScript para gerar o JavaScript
+
+    // tsc -w 
+    // node build/nomeArquivo.js
+
+const nome = "Felipe";
+const idade = 21;
+
+console.log(nome);
+console.log(idade);
